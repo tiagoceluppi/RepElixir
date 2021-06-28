b094f9f5d58149f980c1d0f392a28c5c63659723
TiagoCeluppi <>
2021-06-28 17:13:41 +0000

    Initial commit

---
+++ main.exs
@@ -0,0 +1,22 @@
+#!/usr/bin/env elixir
+
+defmodule Combination do
+
+  def c2(list, _fun) when length(list) < 2, do: :ok
+
+  def c2([a|tail], fun) do
+    Enum.each(tail, fn(b)-> fun.(a, b) end)
+    c2(tail, fun)
+  end
+end
+
+nums = "./list.txt"
+       |> File.stream!([], :line)
+       |> Stream.map(&String.trim/1)
+       |> Enum.map(&String.to_integer/1)
+
+Combination.c2(nums, fn(a, b)->
+  if a + b == 2020 do
+    IO.inspect(a * b, label: "Report Repair Result")
+  end
+end)
